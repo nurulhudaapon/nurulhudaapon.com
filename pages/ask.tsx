01140bd484da537d3716060b0ad02b2e18b1b919
Nurul Huda (Apon) <me@nurulhudaapon.com>
2023-10-23 17:45:27 +0600

    fix: replace ask banner

--- pages/ask.tsx
+++ pages/ask.tsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 export default function Newsletter() {
     const [time, setTime] = useState(new Date());
     return (
-        <Container title="Ask – Nurul Huda (Apon)" description="Anonymously ask me anything" image={"https://nurulhudaapon.com/static/images/ask-banner.jpeg"} >
+        <Container title="Ask – Nurul Huda (Apon)" description="Anonymously ask me anything" image={'https://nurulhudaapon.com/static/images/ask-banner.png'}>
             <div className="mx-auto mb-16 flex max-w-2xl flex-col items-start justify-center">
                 <h1 className="mb-4 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">Ask Me Anything</h1>
                 <p className="mb-4 text-gray-600 dark:text-gray-400">
