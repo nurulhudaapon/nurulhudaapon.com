57ff2134e35e3714fa4464cb910c4c92be51c9ed
Nurul Huda (Apon) <me@nurulhudaapon.com>
2025-05-06 16:06:11 +0100

    refactor: dry layout

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,16 +1,3 @@
-import Navigation from "../../components/navs";
+import { MainLayout } from "@/components/layout";
 
-export default function AboutLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <main className="flex flex-col items-center w-full max-w-4xl mx-auto py-4 sm:py-20 px-4">
-      <div className="w-full max-w-3xl space-y-8">
-        <Navigation />
-        {children}
-      </div>
-    </main>
-  );
-} 
+export default MainLayout;
