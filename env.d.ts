afbb7d76ba398e8924e95061cee705000c316997
Nurul Huda (Apon) <me@nurulhudaapon.com>
2025-05-10 16:55:41 +0600

    ci: update default domain

--- env.d.ts
+++ env.d.ts
@@ -2,7 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     /**
      * The base URL of the website.
-     * @example https://next.nuhu.dev
+     * @example https://nurulhudaapon.com
      */
     NEXT_PUBLIC_BASE_URL: string;
   }
